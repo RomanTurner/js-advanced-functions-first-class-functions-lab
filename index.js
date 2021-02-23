aee48575286dcae14b13b498a3d7c21d152bb8bc
Roman Turner <rn.turner@live.com>
2021-02-22 22:19:28 -0800

    commit tree 91c03cec3dd6bfd38eba2e48a178bd16b7a61094

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+const drivers = ["Sally", "Bob", "Freddy", "Claudia"];
+
+const returnFirstTwoDrivers = (array) => array.slice(0, 2);
+
+console.log(returnFirstTwoDrivers(drivers));
+
+const returnLastTwoDrivers = (array) => array.slice(-2);
+console.log(returnLastTwoDrivers(drivers));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(multiplier){
+    return (fare) => {
+       return fare * multiplier;
+    };
+}
+
+const fareDoubler = (num) => createFareMultiplier(2)(num);
+const fareTripler = (num) => createFareMultiplier(3)(num);
+
+console.log(fareDoubler(8));
+
+function selectDifferentDrivers(drivers, selectingDrivers) {
+    return selectingDrivers(drivers);
+}
